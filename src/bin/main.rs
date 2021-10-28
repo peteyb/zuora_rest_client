9d8f5a2c131c3a671f633e6bcd920784b91df1c5
peteyb <pbryant@peterbryant.me.uk>
2021-10-28 11:16:35 +0100

    feat: get oauth token
    test: add generate token test
    
    feat: add async get method
    
    - construct headers to setup bearer token
    - refactor generate_token to run outisde of constructor
    
    doc: add doc comments
    
    docs: add description

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -2,19 +2,25 @@ use std::env;
 use std::process;
 use zuora_rest_client::Zuora;
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = env::args().collect();
-    if args.len() < 4 {
+    if args.len() < 2 {
         eprintln!("Not enough arguments");
         process::exit(1);
     }
 
-    let client = Zuora::new(
+    let mut client = Zuora::new(
+        env::var("ZUORA_CLIENT_ID").unwrap_or_default(),
+        env::var("ZUORA_CLIENT_SECRET").unwrap_or_default(),
         args[1].clone(),
         args[2].clone(),
-        args[3].clone(),
-        args[4].clone(),
         3,
     );
-    println!("{:?}", client);
+
+    let token = client.generate_token();
+    println!("{:?}", token);
+
+    let get = client.get("/catalog/products", serde_json::from_str("{}").unwrap());
+    println!("{:?}", get);
+    Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,27 @@
+//! # Zuora REST Client
+//!
+//! `zuora_rest_client` is a HTTP Client built on top of the `reqwest` package  for accessing the
+//! [Zuora Billing REST API](https://www.zuora.com/developer/api-reference/)
+//!
+//! This package only currently provides an interface for performing OAuth authenticated GET
+//! requests
+//!
+//! # TODO
+//!
+//! - Add retry logic
+//! - Add missing HTTP methods
+use reqwest::header::{HeaderMap, HeaderValue, AUTHORIZATION};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+struct AccessToken {
+    access_token: String,
+    token_type: String,
+    expires_in: usize,
+    scope: String,
+    jti: String,
+}
+
 #[derive(Debug)]
 pub struct Zuora {
     client_id: String,
@@ -5,8 +29,11 @@ pub struct Zuora {
     domain: String,
     version: String,
     retry_attempts: usize,
+    client: reqwest::Client,
+    token: Option<AccessToken>,
 }
 
+/// Create an instance of a Zuora client
 impl Zuora {
     pub fn new(
         client_id: String,
@@ -14,32 +41,169 @@ impl Zuora {
         domain: String,
         version: String,
         retry_attempts: usize,
-    ) -> Zuora {
-        Zuora {
+    ) -> Self {
+        Self {
             client_id,
             client_secret,
             domain,
             version,
             retry_attempts,
+            client: reqwest::Client::new(),
+            token: None,
         }
     }
     fn endpoint(&self) -> String {
         format!("{}{}", self.domain, self.version)
+    }
+
+    fn construct_headers(&self) -> HeaderMap {
+        let mut headers = HeaderMap::new();
+
+        if let Some(token) = &self.token {
+            let bearer = format!("Bearer {}", { &token.access_token });
+            headers.insert(
+                AUTHORIZATION,
+                HeaderValue::from_str(bearer.as_str()).unwrap(),
+            );
+        }
+
+        headers
+    }
+    /// Generate and set an OAuth token against this instance of the Zuora client to allow bearer
+    /// token in subsequent HTTP requests
+    ///
+    /// # Errors
+    ///
+    /// This method may return a `reqwest::Error` where the call to Zuora was not successful
+    ///
+    #[tokio::main]
+    pub async fn generate_token(&mut self) -> Result<(), reqwest::Error> {
+        let request_url = format!("{}/oauth/token", self.domain);
+
+        let data = [
+            ("client_id", self.client_id.as_str()),
+            ("client_secret", self.client_secret.as_str()),
+            ("grant_type", "client_credentials"),
+        ];
+
+        let resp = self.client.post(&request_url).form(&data).send().await;
+        match resp {
+            Ok(x) if x.status().is_success() => {
+                let data = x.text().await.unwrap();
+                self.token = Some(serde_json::from_str(&data[..]).unwrap());
+                Ok(())
+            }
+            Ok(_x) => Ok(()),
+            Err(err) => Err(err),
+        }
+    }
+    /// Perform GET request on Zuora API
+    ///
+    /// # Example
+    ///
+    /// ```ignore
+    /// let get = client.get("/catalog/products", serde_json::from_str("{}").unwrap());
+    /// println!("{:?}", get);
+    /// ```
+    /// # Errors
+    ///
+    /// This method may return a `reqwest::Error` where the call to Zuora was not successful
+    ///
+    #[tokio::main]
+    pub async fn get(
+        &self,
+        path: &str,
+        payload: serde_json::Value,
+    ) -> Result<serde_json::Value, reqwest::Error> {
+        let resp = self
+            .client
+            .get(self.endpoint() + path)
+            .headers(self.construct_headers())
+            .form(&payload)
+            .send()
+            .await;
+        match resp {
+            Ok(x) => {
+                let data = x.text().await.unwrap();
+                let value = serde_json::from_str(&data[..]).unwrap();
+                Ok(value)
+            }
+            Err(err) => Err(err),
+        }
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    #[test]
-    fn client_endpoint() {
-        let client = Zuora::new(
+    use mockito;
+    use mockito::mock;
+    fn init() -> Zuora {
+        let host = mockito::server_url();
+        Zuora::new(
             String::from("client_id"),
             String::from("client_secret"),
-            String::from("https://rest.sandbox.eu.zuora.com"),
+            host,
             String::from("/v1"),
             3,
-        );
-        assert_eq!(client.endpoint(), "https://rest.sandbox.eu.zuora.com/v1");
+        )
+    }
+    #[test]
+    fn endpoint() {
+        let client = init();
+        assert_eq!(client.endpoint(), format!("{}/v1", client.domain));
+    }
+
+    #[test]
+    fn construct_headers_default() {
+        let client = init();
+        let headers = client.construct_headers();
+        assert_eq!(headers.len(), 0);
+    }
+
+    #[test]
+    fn construct_headers_auth() {
+        let mut client = init();
+        let token = AccessToken {
+            access_token: String::from("access"),
+            token_type: String::from("bearer"),
+            expires_in: 100,
+            scope: String::from("scope"),
+            jti: String::from("jti"),
+        };
+        client.token = Some(token);
+        let headers = client.construct_headers();
+        assert!(headers.contains_key(AUTHORIZATION));
+        assert_eq!(headers[AUTHORIZATION], "Bearer access");
+    }
+
+    #[test]
+    fn generate_token() {
+        let mock_request = mock("POST", "/oauth/token")
+            .with_status(200)
+            .with_header("content-type", "application/json")
+            .with_body(
+                r#"{ 
+                "access_token": "access", 
+                "token_type": "bearer", 
+                "expires_in": 100, 
+                "scope": "scope", 
+                "jti": "jti" 
+            }"#,
+            )
+            .create();
+
+        let mut client = init();
+        let token = AccessToken {
+            access_token: String::from("access"),
+            token_type: String::from("bearer"),
+            expires_in: 100,
+            scope: String::from("scope"),
+            jti: String::from("jti"),
+        };
+        let result = client.generate_token();
+        assert_eq!(result.unwrap(), ());
+        assert_eq!(client.token, Some(token));
+        mock_request.assert();
     }
 }
