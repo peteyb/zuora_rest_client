d88c580f5f106666f105a5fc99093fa2944b7b0f
peteyb <pbryant@peterbryant.me.uk>
2021-10-28 11:16:35 +0100

    feat: get oauth token

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,20 +1,32 @@
+// use std::collections::HashMap;
 use std::env;
 use std::process;
 use zuora_rest_client::Zuora;
 
-fn main() {
+// #[tokio::main]
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = env::args().collect();
-    if args.len() < 4 {
+    if args.len() < 2 {
         eprintln!("Not enough arguments");
         process::exit(1);
     }
 
     let client = Zuora::new(
+        env::var("ZUORA_CLIENT_ID").unwrap_or_default(),
+        env::var("ZUORA_CLIENT_SECRET").unwrap_or_default(),
         args[1].clone(),
         args[2].clone(),
-        args[3].clone(),
-        args[4].clone(),
         3,
     );
-    println!("{:?}", client);
+    // println!("{:?}", client);
+
+    let token = client.generate_token();
+    println!("{:?}", token);
+
+    // let resp = reqwest::get("https://httpbin.org/ip")
+    //     .await?
+    //     .json::<HashMap<String, String>>()
+    //     .await?;
+    // println!("{:#?}", resp);
+    Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,24 @@
+use reqwest::{
+    header::{HeaderMap, HeaderValue, CONTENT_TYPE},
+    Client, ClientBuilder,
+};
+use serde::{Deserialize, Serialize};
+use serde_json;
+use std::collections::HashMap;
+use std::future::ready;
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct AccessToken {
+    access_token: String,
+    token_type: String,
+    expires_in: usize,
+    scope: String,
+    jti: String,
+}
+
 #[derive(Debug)]
 pub struct Zuora {
+    client: reqwest::Client,
     client_id: String,
     client_secret: String,
     domain: String,
@@ -14,8 +33,9 @@ impl Zuora {
         domain: String,
         version: String,
         retry_attempts: usize,
-    ) -> Zuora {
-        Zuora {
+    ) -> Self {
+        Self {
+            client: reqwest::Client::new(),
             client_id,
             client_secret,
             domain,
@@ -25,6 +45,26 @@ impl Zuora {
     }
     fn endpoint(&self) -> String {
         format!("{}{}", self.domain, self.version)
+    }
+    #[tokio::main]
+    pub async fn generate_token(&self) -> Result<AccessToken, reqwest::Error> {
+        let request_url = format!("{}/oauth/token", self.domain);
+
+        let data = [
+            ("client_id", self.client_id.as_str()),
+            ("client_secret", self.client_secret.as_str()),
+            ("grant_type", "client_credentials"),
+        ];
+
+        let resp = self.client.post(&request_url).form(&data).send().await;
+        match resp {
+            Ok(x) => {
+                let data = x.text().await.unwrap();
+                let token: AccessToken = serde_json::from_str(&data[..]).unwrap();
+                Ok(token)
+            }
+            Err(err) => Err(err),
+        }
     }
 }
 
